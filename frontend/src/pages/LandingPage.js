1c9ad16375e50024a3e946be98a50debca7c96bf
ConnerKT <connerthompson121@gmail.com>
2023-12-11 08:20:26 -0600

    added simple Landing Page
    Added particleJS for a future landing page
    Added TailwindCSS for a simplified designing process

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function LandingPage() {
   return (
-    <div>
-      <h1>Welcome to the landing page!!!!</h1>
+    <div className="flex items-center justify-center h-screen flex-col text-center space-y-4">
+      <h1>Oasis</h1>
+      <p className="font-kbbubblegum">A comfy platform to chat with friends and make friends!</p>
+      <button className="bg-customColor hover:bg-customColorDark text-white font-bold py-2 px-4 rounded-full">
+        Try Now
+      </button>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
